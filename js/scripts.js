dbd256bc93f132ab6450ed4a734b20634d54f51f
cici harrison and zoe zhou <cici@>
2018-07-02 20:31:18 -0700

    it will create a ticket construct that creates new ticket objects, and a prototype that prints all information - ticket qty, movie name, time slot and total cost

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,44 @@
 //Business Logic
+function Ticket(movie, time, quantity) {
+  this.movieName = movie;
+  this.timeSlot = time;
+  this.quantityNum = quantity;
+}
 
-
-
+Ticket.prototype.info = function() {
+  return this.quantityNum + " tickets for " + this.movieName + " at " + this.timeSlot;
+}
 
 
 //User Interface Logic
 $(document).ready(function() {
-  $("form#info").submit(function(event) {
+  $("form#step2").submit(function(event) {
     event.preventDefault();
 
-    var nemo = parseInt($("#nemo option:selected").val());
-    var benji = parseInt($("#benji option:selected").val());
-    var cats = parseInt($("#cats option:selected").val());
+    var movieName = $("#movie option:selected").text();
+    var timeSlot = $("#time option:selected").text();
 
-    var movieCost = nemo + benji + cats;
+    //console.log("movie name: " + movieName);
+    //console.log("time slot: " + timeSlot);
+
+    var movieCost = parseInt($("#movie option:selected").val());
+    var timeCost = parseInt($("#time option:selected").val());
     var ageCost = parseInt($("input[name=age]:checked").val());
     var ticketQty = parseInt($("#qty").val());
 
-    var totalCost = (movieCost + ageCost)*ticketQty;
+    var totalCost = (movieCost + timeCost + ageCost)*ticketQty;
 
-    console.log(movieCost);
-    console.log(ageCost);
-    console.log(ticketQty);
-    console.log(totalCost);
+    var newTicket = new Ticket(movieName, timeSlot, ticketQty);
 
-    $("h2#total").text("$"+totalCost+".00");
+    console.log(newTicket.info());
+
+    //console.log("movie " + movieCost);
+    //console.log("time " + timeCost);
+    //console.log("age " + ageCost);
+    //console.log("qty " + ticketQty);
+    //console.log("total " + totalCost);
+
+    $("h2#total").text(newTicket.info() + " for " + "$" + totalCost + ".00");
 
   });
 });
